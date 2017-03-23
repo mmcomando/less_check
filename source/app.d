ca5c3c5d642d9988373de1c12c0bcd06e7784ea8
mmcomando <mmcomando@gmail.com>
2017-03-23 17:52:51 +0100

    Command line interface

--- source/app.d
+++ source/app.d
@@ -7,9 +7,47 @@ import std.algorithm:sort;
 import tokenizer;
 import ast_check;
 
-void main()
-{
+void main(string[] args){
+	foreach(arg;args[1..$]){
+		if(arg=="--help"){
+			printHelp();
+		}else if(arg=="--test"){
+			test();
+		}else{
+			checkFile(arg);
+		}
+	}
+	debug{
+		test();
+	}
 
+}
+
+void checkFile(string filePath){
+	string content = readText(filePath);
+	AstCheck ast=new AstCheck(content);
+	try{
+		ast.check();
+		writeln("Syntax is OK.");
+	}catch(Exception e){
+		ast.tokenizer.printError(e.msg);
+		debug writeln(e);
+	}catch(RangeError e){
+		ast.tokenizer.printError("Range violation near line.");
+	}
+}
+
+void printHelp(){
+	writeln(
+"less_check - program to check less scripts syntax
+Usage:
+less_check [--help] [--test] [file_list]
+--help - displays this help
+--test - run internal tests
+file_list - files to check syntax, separated by space
+");
+}
+void test(){
 	string[] tests=[
 		"
 @base:#f04615;
@@ -19,7 +57,7 @@ void main()
 }
 
 @xx:20px;
-
+/* dfsgdfgdfg sdfgsdf gsdfgsdf gsdf gsdf gsdgsdfgsdfgsd */
 
 /* One hell of a block style comment! */@var: red; // Get in line!
 
@@ -53,31 +91,22 @@ ala:asda;
 				ast.check();
 			}catch(Exception e){
 				ast.tokenizer.printError(e.msg);
+				writeln(e);
 			}catch(RangeError e){
 				ast.tokenizer.printError("Range violation near line.");
 			}
 		}
-
+		
 		
 		string[] files;
 		foreach (string testFileName;dirEntries("less_tests", SpanMode.shallow))
 		{
 			files~=testFileName;
 		}
-		foreach (i,string testFileName;files.sort[0..6])
+		foreach (i,string testFileName;files.sort)
 		{
-			string content = readText(testFileName);
-			AstCheck ast=new AstCheck(content);
-			try{
-				writeln(testFileName,": ----------- ",i+1," -----------");
-				ast.check();
-			}catch(Exception e){
-				ast.tokenizer.printError(e.msg);
-				//writeln(e);
-			}catch(RangeError e){
-				ast.tokenizer.printError("Range violation near line.");
-			}
+			writeln(testFileName,": ----------- ",i+1," -----------");
+			checkFile(testFileName);
 		}
 	}
 }
-
